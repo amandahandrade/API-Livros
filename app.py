eb045530d47204c999d5aab4dfb5c8a970252e7f
amandahandrade <amandah.andrades@gmail.com>
2025-03-26 15:08:35 -0300

    adicionando requirements.txt e configuração do CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import sqlite3
-from flask import Flask, request, jsonify, json  
+from flask import Flask, request, jsonify, json
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+CORS(app)
 
 @app.route("/")
 def oAlho():
